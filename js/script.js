9ca0bb4dffb295749346b5f3f042dbe6b5fe9d8a
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-04-07 14:05:27 +0200

    richiamo l'api

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 var root = new Vue({
 el: '#root',
  data: {
+   emailList: [],
 
     },
-   mounted: {
+   mounted: function() {
+     select = this;
+     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(function (response) {
+      
 
-   },
+
+    });
+   }
+
+
+   ,
    methods: {
 
    }
