7ab77f468560acf36ee1ff2c745c93247412761d
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-04-07 14:42:19 +0200

    pusho il  risultato nell'array vuoto e stampo a schermo la lista

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ el: '#root',
      for (var i = 0; i < 10 ; i++) {
          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
          .then( function (response) {
-          console.log(response.data.response);
+          // console.log(response.data.response);
+          const resultCall = response.data.response;
+          select.emailList.push(resultCall);
          });
      }
    },
